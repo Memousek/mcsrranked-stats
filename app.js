6bbe679d25d8a148dc1749fdf067a2b185e22af6
Memousek <namiskoczech@gmail.com>
2023-05-20 23:59:01 +0200

    yyy

--- app.js
+++ app.js
@@ -36,30 +36,30 @@
       },
       addRank(elo) {
         return elo > 0 && elo < 199
-          ? "coal I"
-          : elo > 200 && elo < 399
-          ? "coal II"
-          : elo > 400 && elo < 599
-          ? "coal III"
-          : elo > 600 && elo < 799
-          ? "iron I"
-          : elo > 800 && elo < 999
-          ? "iron II"
-          : elo > 1000 && elo < 1199
-          ? "iron III"
-          : elo > 1200 && elo < 1399
-          ? "gold I"
-          : elo > 1400 && elo < 1599
-          ? "gold II"
-          : elo > 1600 && elo < 1799
-          ? "gold III"
-          : elo > 1800 && elo < 1999
-          ? "diamond I"
-          : (elo > 2000) & (elo < 2199)
-          ? "diamond II"
-          : elo > 2200 && elo < 2399
-          ? "diamond III"
-          : "";
+            ? "Coal I"
+            : elo > 200 && elo < 399
+            ? "Coal II"
+            : elo > 400 && elo < 599
+            ? "Coal III"
+            : elo > 600 && elo < 799
+            ? "Iron I"
+            : elo > 800 && elo < 999
+            ? "Iron II"
+            : elo > 1000 && elo < 1199
+            ? "Iron III"
+            : elo > 1200 && elo < 1399
+            ? "Gold I"
+            : elo > 1400 && elo < 1599
+            ? "Gold II"
+            : elo > 1600 && elo < 1799
+            ? "Gold III"
+            : elo > 1800 && elo < 1999
+            ? "Diamond I"
+            : elo > 2000 && elo < 2199
+            ? "Diamond II"
+            : elo > 2200 && elo < 2399
+            ? "Diamond III"
+            : "Unranked";
     },
     formatTime(timeInMs) {
         const minutes = Math.floor(timeInMs / 60000);
